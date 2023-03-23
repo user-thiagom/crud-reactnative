d2b7ffd28cfedde0428c6c1947880a6ee91494b1
Thiago Mateus <thiagomateus123@hotmail.com>
2023-03-23 10:43:53 -0300

    Criando Alguns dados Mockados

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id:1,
+        name: 'Thiago Marteus',
+        email:'thiago@gmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+    {
+        id:2,
+        name: 'Ana Julia',
+        email:'a.julia10@gmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png'
+    },
+    {
+        id:3,
+        name: 'Rubens Nigro',
+        email:'nigrorub2022@gmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+    },
+    {
+        id:4,
+        name: 'Cátia Fernandes',
+        email:'catfernandes@gmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+    }
+]
